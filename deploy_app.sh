9f933248a269d2e07b72d4f9c46fd74c9c811f0c
Guillermo Buritica T <gtobon@whispir.com>
2018-06-13 15:56:58 +1000

    Change fake Key

--- deploy_app.sh
+++ deploy_app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-aws ec2 run-instances --count 1 --image-id ami-67589505 --instance-type  t2.micro --key-name whispir-common --placement AvailabilityZone=ap-southeast-2b --subnet-id subnet-79adca1d --user-data file:application.sh
+aws ec2 run-instances --count 1 --image-id ami-67589505 --instance-type  t2.micro --key-name common.pem --placement AvailabilityZone=ap-southeast-2b --subnet-id subnet-79adca1d --user-data file:application.sh
 
